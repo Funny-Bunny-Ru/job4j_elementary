8ffd505cc385d781e1220c6a618042ff543bd91d
Funny-Buny <incoming@mail.ru>
2021-05-19 11:22:07 +0300

    1. Определите тип данных для переменных. [#172634]

---
+++ src/ru/job4j/ru/job4j/Homework1.java
@@ -0,0 +1,30 @@
+package ru.job4j.ru.job4j;
+
+public class Homework1 {
+    public static void main (String[] args) {
+        // number of full age of the men / количество полных лет человека
+        byte age = 35;
+        System.out.println(age);
+        // gender of the person / пол человека
+        String S1 = "male";
+        System.out.println(S1);
+        // height of person in metres / рост человека в метрах
+        float height = 1.82F;
+        System.out.println(height);
+        // number of bricks for building house / количество кирпичей для постройки дома (штук)
+        int bricks = 58765;
+        System.out.println(bricks);
+        // number of bacterias / количество бактерий
+        long bacterias = 535481254805L;
+        System.out.println(bacterias);
+        // distance to space objects / расстояниме до космических объектов
+        double space = 745654.455;
+        System.out.println(space);
+        // work permission / разрешение на работу
+        String S2 = "Yes";
+        System.out.println(S2);
+        // file size / размер файла
+        float file = 38.65F;
+        System.out.println(file);
+    }
+}
