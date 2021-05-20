75c210a8d6dde76cff4236aeb26403e34e71831d
Funny-Buny <incoming@mail.ru>
2021-05-20 11:59:40 +0300

    4.1. Аргументы [#295521/0]

---
+++ src/ru/job4j/ru/job4j/ArgMethod.java
@@ -0,0 +1,17 @@
+package ru.job4j.ru.job4j;
+
+public class ArgMethod {
+    public static void hello(String name, int age) {
+        System.out.println(name);
+        System.out.println(name + age);
+        System.out.println(age);
+        System.out.println(name + name + name);
+    }
+    public static void main(String[] args) {
+        String name = "Petr Arsentev ";
+        int age = 33;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(age);
+        ArgMethod.hello(name, name, name);
+    }
+}
