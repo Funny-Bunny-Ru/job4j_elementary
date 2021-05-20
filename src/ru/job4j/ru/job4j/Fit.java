284ca885412f210612dbe51d872ccc73886f0959
Funny-Buny <incoming@mail.ru>
2021-05-20 17:26:51 +0300

    6. Идеальный вес [#41584 #158658/2]

--- src/ru/job4j/ru/job4j/Fit.java
+++ src/ru/job4j/ru/job4j/Fit.java
@@ -8,7 +8,7 @@ public class Fit {
         return (double)(height - 110) * 1.15;
     }
     public static void main (String[] args) {
-        short height = 183;
+        short height = 188;
         double man = Fit.manWeight(height);
         double woman = Fit.womanWeight(height);
         System.out.println("Man 183 is " + man);
