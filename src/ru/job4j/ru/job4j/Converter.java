4ea9bc980b82634103c00ca5beabc6ce5f8b8a39
Funny-Buny <incoming@mail.ru>
2021-05-20 15:07:56 +0300

    5. Конвертер валюты [#41583]

---
+++ src/ru/job4j/ru/job4j/Converter.java
@@ -0,0 +1,43 @@
+package ru.job4j.ru.job4j;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return (float) rsl;
+    }
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60;
+        return (float) rsl;
+    }
+    public static float euroToDollar(float value) {
+        float rsl = value * 70 / 60;
+        return (float) rsl;
+    }
+    public static float euroToRuble(float value) {
+        float rsl = value * 70;
+        return (float) rsl;
+    }
+    public static float dollarToEuro(float value) {
+        float rsl = value * 60 / 70;
+        return (float) rsl;
+    }
+    public static float dollarToRuble(float value) {
+        float rsl = value * 60;
+        return (float) rsl;
+    }
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        float dollar = Converter.rubleToDollar(140);
+        float dollar1 = Converter.euroToDollar(140);
+        float ruble = Converter.euroToRuble(140);
+        float euro1 = Converter.dollarToEuro(140);
+        float ruble1 = Converter.dollarToRuble(140);
+        System.out.println("140 rubles " + euro + " euro");
+        System.out.println("140 rubles " + dollar + " dollars");
+        System.out.println("140 euro " + dollar1 + " dollars");
+        System.out.println("140 euro " + ruble + " rubles");
+        System.out.println("140 dollars " + euro1 + " euro");
+        System.out.println("140 dollars " + ruble1 + " rubles");
+
+    }
+}
