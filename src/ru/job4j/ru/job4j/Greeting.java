4fca4d8c5f001996d4196584a22bac4acde2e0a7
Funny-Buny <incoming@mail.ru>
2021-05-22 13:58:47 +0300

    9.1. String [#237407 #159339]

--- src/ru/job4j/ru/job4j/Greeting.java
+++ src/ru/job4j/ru/job4j/Greeting.java
@@ -3,9 +3,8 @@ package ru.job4j.ru.job4j;
 public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java! ";
-        String idea1 = "But I am newbie ";
         int year = 2021;
         System.out.println(idea);
-        System.out.println(idea1 + "in " + year + " year.");
+        System.out.println("But I am newbie in " + year + " year.");
     }
 }
