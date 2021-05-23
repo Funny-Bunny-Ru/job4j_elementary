1a0f1fd042f934150b6bb45904952be57970cb6c
Funny-Buny <incoming@mail.ru>
2021-05-23 17:14:05 +0300

    1.1. Что такое import [#504773]

---
+++ src/ru/job4j/math/MathFunction.java
@@ -0,0 +1,19 @@
+package ru.job4j.math;
+
+public class MathFunction {
+    public static double sumNumbers(double first, double second) {
+        return first + second;
+    }
+
+    public static double timeNumbers(double first, double second) {
+        return first * second;
+    }
+
+    public static double difNumbers(double first, double second) {
+        return first - second;
+    }
+
+    public static double divNumber(double first, double second) {
+        return first / second;
+    }
+}
--- src/ru/job4j/ru/job4j/Calculator.java
+++ src/ru/job4j/ru/job4j/Calculator.java
@@ -1,8 +1,11 @@
 package ru.job4j.ru.job4j;
 
+import static ru.job4j.math.MathFunction.*;
+
 public class Calculator {
+
     public static void main(String[] args) {
-        int one =1;
+        int one = 1;
         int two = 2;
         int four = 4;
         int five = 5;
@@ -13,6 +16,20 @@ public class Calculator {
         System.out.println(fiveMinusTwo);
         int fourTimeTwo = four * two;
         System.out.println(fourTimeTwo);
+        System.out.println("Результат расчета равен: " + sumAndTime(10, 20));
+        System.out.println("Результат расчета разность плюс деление равен: " + sumAndDiv(10, 20));
+        System.out.println("Результат сложения всех действий: " + sumAll(10, 20));
+    }
 
+    public static double sumAndTime(double first, double second) {
+        return sumNumbers(first, second) + timeNumbers(first, second);
+    }
+
+    public static double sumAndDiv(double first, double second) {
+        return difNumbers(first, second) + divNumber(first, second);
+    }
+
+    public static double sumAll(double first, double second) {
+        return sumNumbers(first, second) + timeNumbers(first, second) + difNumbers(first, second) + divNumber(first, second);
     }
 }
