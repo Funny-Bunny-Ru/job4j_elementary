2de732dddbe7d98068f34f1caeb62ee101f31c67
Funny-Buny <incoming@mail.ru>
2021-06-09 09:15:10 +0300

    6.1. Классический поиск перебором. [#33489 #165855]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -24,4 +24,13 @@ public class FindLoopTest {
         int expected = 2;
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void whenArrayHas8ThenFalse() {
+        int[] data = {5, 4, 3, 2};
+        int el = 8;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
 }
